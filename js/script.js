4615b969339016fd8d0a62e59d2f70dc9285ba69
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-04-18 19:14:07 -0500

    Create getComputerChoice()

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const OPTIONS = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    randomNumber = Math.floor(Math.random() * OPTIONS.length);
+
+    return OPTIONS[randomNumber]; 
+}
