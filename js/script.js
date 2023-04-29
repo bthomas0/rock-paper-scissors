f12470548412b0b5f07cfc14cb02a33f22d51e17
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-04-29 15:23:16 -0500

    Refactor playRound() with if/else statements and to return strings

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 const WEAPONS = ['rock', 'paper', 'scissors'];
-let playerPoints = 0;
-let computerPoints = 0;
-let roundCount = 1;
+
 
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * WEAPONS.length);
@@ -13,84 +11,85 @@ function getPlayerChoice() {
     const buttons = document.querySelectorAll('.button');
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
-            console.log(button.id);
-            return button.id;
+            playRound(button.id, getComputerChoice());
         });
     });
+};
+
+function playRound(playerSelection, computerSelection) {
+    const drawMsg = `DRAW! (You both selected ${playerSelection})`;
+    const defeatMsg = `DEFEAT! (${playerSelection} < ${computerSelection})`;
+    const victoryMsg = `VICTORY! (${playerSelection} > ${computerSelection})`;
+    
+    if (playerSelection === 'quit') {
+        console.log('quit');
+        return 'quit';
+    }
+    if (playerSelection === computerSelection) {
+        console.log(drawMsg);
+    
+    // Player selects rock
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            console.log(defeatMsg);
+            return 'lose';
+        } else {
+            console.log(victoryMsg);
+            return 'win';
+        }
+    
+    // Player selects paper
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            console.log(defeatMsg);
+            return 'lose';
+        } else {
+            console.log(victoryMsg);
+            return 'win';
+        }
+    
+    // Player selects scissors
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            console.log(defeatMsg);
+            return 'lose';
+        } else {
+            console.log(victoryMsg)
+            return 'win';
+        }
+    }
 }
 
-// function getPlayerChoice() {
-//     while (true) {
-//         let playerChoice = prompt('Choose your weapon:', '');
-//         if (playerChoice === null) {
-//             confirmCancel = confirm('Are you sure you want to surrender?');
-//             if (confirmCancel) {
-//                 return 'quit';
+// function game() {
+//     let keepPlaying = true;
+//     while (keepPlaying) { 
+//         if (roundCount <= 5) {  
+//             if (playRound(getPlayerChoice(), getComputerChoice()) === 'quit') {
+//                 quitGame();
+//                 return 'quit'
 //             }
-//         } else if (playerChoice.toLowerCase() === 'rock' || playerChoice.toLowerCase() === 'paper' || playerChoice.toLowerCase() === 'scissors') {
-//             return playerChoice.toLowerCase()
-//         } else {
-//             alert('You must select "rock" "paper" or "scissors"');
+//             else {
+//                 console.log(`Score: ${playerPoints} - ${computerPoints}\n\n`);
+//                 roundCount += 1;
+//             }
+//         }
+//         else {
+//             keepPlaying = false;
+//             console.log('Final Results:');
+//             showFinalScore();
 //         }
 //     }
 // }
 
-function playRound(playerSelection, computerSelection) {
-    const matchInfo = `ROUND ${roundCount}: ${playerSelection} vs. ${computerSelection}`;
-    const drawMsg = `DRAW! (You both selected ${playerSelection})`;
-    const defeatMsg = `DEFEAT! (${playerSelection} < ${computerSelection})`;
-    const victoryMsg = `VICTORY! (${playerSelection} > ${computerSelection})`;
-    if (playerSelection === 'quit') {
-        return 'quit';
-    }
-    if (playerSelection === computerSelection) {
-        console.log(matchInfo);
-        console.log(drawMsg);
-
-    } else if (playerSelection === 'rock') {
-        console.log(matchInfo);
-        (computerSelection === 'paper') ? (console.log(defeatMsg), addPoint('computer')) : (console.log(victoryMsg), addPoint('player'));
-   
-    } else if (playerSelection === 'paper') {
-        console.log(matchInfo);
-        (computerSelection === 'scissors') ? (console.log(defeatMsg), addPoint('computer')) : (console.log(victoryMsg), addPoint('player'));
-    
-    } else if (playerSelection === 'scissors') {
-        console.log(matchInfo);
-        (computerSelection === 'rock') ? (console.log(defeatMsg), addPoint('computer')) : (console.log(victoryMsg), addPoint('player'));
-    }
-}
-
-function game() {
-    let keepPlaying = true;
-    while (keepPlaying) { 
-        if (roundCount <= 5) {  
-            if (playRound(getPlayerChoice(), getComputerChoice()) === 'quit') {
-                quitGame();
-                return 'quit'
-            }
-            else {
-                console.log(`Score: ${playerPoints} - ${computerPoints}\n\n`);
-                roundCount += 1;
-            }
-        }
-        else {
-            keepPlaying = false;
-            console.log('Final Results:');
-            showFinalScore();
-        }
-    }
-}
-
-function showFinalScore() {
-    if (playerPoints === computerPoints) {
-        console.log(`IT'S A DRAW! (Final score is ${playerPoints} - ${computerPoints})`);
-    } else if (playerPoints > computerPoints) {
-        console.log(`PLAYER WINS! (Final score is ${playerPoints} - ${computerPoints})`);
-    } else {
-        console.log(`PLAYER LOSES! (Final score is ${playerPoints} - ${computerPoints})`);
-    }
-}
+// function showFinalScore() {
+//     if (playerPoints === computerPoints) {
+//         console.log(`IT'S A DRAW! (Final score is ${playerPoints} - ${computerPoints})`);
+//     } else if (playerPoints > computerPoints) {
+//         console.log(`PLAYER WINS! (Final score is ${playerPoints} - ${computerPoints})`);
+//     } else {
+//         console.log(`PLAYER LOSES! (Final score is ${playerPoints} - ${computerPoints})`);
+//     }
+// }
 
 function addPoint(winner) {
     
@@ -107,5 +106,15 @@ function quitGame() {
     alert('YOU QUIT! YOU LOSE THE GAME!');
 }
 
-getPlayerChoice()
-//game();
+function gameLoop() {
+    let playerPoints = 0;
+    let computerPoints = 0;
+    let roundCount = 1;
+
+    while (playerPoints < 5 && computerPoints < 5) {
+        getPlayerChoice();
+        playerPoints += 1;
+    }    
+}
+
+getPlayerChoice();
