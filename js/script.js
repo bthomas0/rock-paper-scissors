2c7fd07b044b4f75c0f8a9d80d218f671f0d0707
bthomas0 <126216659+bthomas0@users.noreply.github.com>
2023-05-03 11:16:00 -0500

    Change control loop to check for points after round

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 /* TODO:
 * handle 'quit'
 * handle 'end of game'
+* play again?
 * 
-*
-*
 *
 *
 */
 
-
 const WEAPONS = ['rock', 'paper', 'scissors'];
 const output = document.getElementById('output');
 const score = document.getElementById('score');
+const originalLayout = document.getElementById('layout');
+const layout = document.getElementById('layout');
 let playerPoints = 0;
 let computerPoints = 0;
 
@@ -53,14 +53,18 @@ function displayWinScenario(message) {
     output.className = 'win';
     console.log(message);
     addPoint('player');
-    displayScore();
+    if (playerPoints >= 5) {
+        displayEndOfGameScenario();
+    } else displayScore();
 }
 
 function displayLoseScenario(message) {
     output.textContent = message;
     output.className = 'lose';
     addPoint('computer');
-    displayScore();
+    if (computerPoints >= 5) {
+        displayEndOfGameScenario();
+    } else displayScore();
 }
 
 function displayDrawScenario(message) {
@@ -72,15 +76,15 @@ function displayDrawScenario(message) {
 function displayQuitScenario() {
     if (confirm('Are you sure you want to quit?')) {
         console.log('yes');
-        // go to end of game scenario
+        // go to end of game (losing) scenario
     }
 }
 
-function displayFinalScore() {
+function displayEndOfGameScenario() {
     if (playerPoints > computerPoints) {
-        console.log(`YOU WIN! (Final score is ${playerPoints} - ${computerPoints})`);
+        layout.textContent = `YOU WIN! (Final score is ${playerPoints} - ${computerPoints})`;
     } else {
-        console.log(`YOU LOSE! (Final score is ${playerPoints} - ${computerPoints})`);
+        layout.textContent = `YOU LOSE! (Final score is ${playerPoints} - ${computerPoints})`;
     }
 }
 
@@ -88,40 +92,33 @@ function playRound(playerSelection, computerSelection) {
     const drawMsg = `DRAW! (You both selected ${playerSelection})`;
     const defeatMsg = `DEFEAT! (${playerSelection} < ${computerSelection})`;
     const victoryMsg = `VICTORY! (${playerSelection} > ${computerSelection})`;
-    
-    if (evalGameContinue() === true) {
 
-        if (playerSelection === 'quit') {
-            displayQuitScenario()
+    if (playerSelection === 'quit') {
+        displayQuitScenario()
+    }
+    if (playerSelection === computerSelection) {
+        displayDrawScenario(drawMsg);
+    // Player selects rock
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            displayLoseScenario(defeatMsg);
+        } else {
+            displayWinScenario(victoryMsg);
         }
-        if (playerSelection === computerSelection) {
-            displayDrawScenario(drawMsg);
-        // Player selects rock
-        } else if (playerSelection === 'rock') {
-            if (computerSelection === 'paper') {
-                displayLoseScenario(defeatMsg);
-            } else {
-                displayWinScenario(victoryMsg);
-            }
-        // Player selects paper
-        } else if (playerSelection === 'paper') {
-            if (computerSelection === 'scissors') {
-                displayLoseScenario(defeatMsg);
-            } else {
-                displayWinScenario(victoryMsg);
-            }
-        // Player selects scissors
-        } else if (playerSelection === 'scissors') {
-            if (computerSelection === 'rock') {
-                displayLoseScenario(defeatMsg);
-            } else {
-                displayWinScenario(victoryMsg);
-            }
+    // Player selects paper
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            displayLoseScenario(defeatMsg);
+        } else {
+            displayWinScenario(victoryMsg);
         }
-
-    } else {
-        displayFinalScore(); 
-        //ask to play again }       
+    // Player selects scissors
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            displayLoseScenario(defeatMsg);
+        } else {
+            displayWinScenario(victoryMsg);
+        }
     }
 }
 
